3aa691d2e44b9793a32d277c67eabef5c9aabc71
Marc Andres <marcandres0923@gmail.com>
2025-08-13 23:48:22 +0800

    testing logout functionality

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Login />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </Router>
